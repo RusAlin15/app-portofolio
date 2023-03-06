8ca0d9fc8beb61847e851340e10e9d1ec8d0e6d1
Alin Rus El-Hassaina <alin.rus.el-hassaina@continental-corporation.com>
2023-03-06 18:55:39 +0200

    render the app titles in two columns

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas
+
 st.set_page_config(layout="wide")
+
 column1, column2  = st.columns(2)
 
 with column1:
@@ -11,6 +14,22 @@ with column2:
     I had improved a lot of skills and competences through my involvement in different extracurricular activities and from work experience:
 - Had the opportunity to interact with many people with different passions, thoughts and different cultural backgrounds
 - Gained a lot of experience in team working
-- Developed a
     """
-    st.text(content)
+    st.write(content)
+
+content2 = """
+"Below you cand find some of the apps I have built in Python. Feel free to contact me!"
+"""
+st.write(content2)
+
+column3, column4  = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep = ';')
+with column3:
+    for index, row in df[:int((len(df)/2))].iterrows():
+        st.header(row['title'])
+
+with column4:
+    for index, row in df[int((len(df)/2)):].iterrows():
+        st.header(row['title'])
+
